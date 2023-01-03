5960276ff44e010d671a29bc14edbd9568b6b1b8
mutuajames <njorog3.james@gmail.com>
2023-01-03 13:02:05 +0300

    webhooks

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='subscriptions-home'),
     path('config/', views.stripe_config, name='stripe-config'),
     path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
+    path('webhook/', views.stripe_webhook, name='stripe-webhook'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,22 +1,31 @@
 import stripe
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from django.contrib.auth.models import User
+
+from .models import StripeCustomer
 
 
 @login_required
 def home(request):
     return render(request, 'home.html', {})
 
+@login_required
+def success(request):
+    return render(request, 'success.html', {})
+
+@login_required
+def cancel(request):
+    return render(request, 'cancel.html', {})
 
 @csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
-
 
 @csrf_exempt
 def create_checkout_session(request):
@@ -41,3 +50,39 @@ def create_checkout_session(request):
         except Exception as e:
             return JsonResponse({'error': str(e)})
         
+@csrf_exempt
+def stripe_webhook(request):
+  stripe.api_key = settings.STRIPE_SECRET_KEY
+  endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+  payload = request.body
+  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+  event = None
+
+  try:
+    event = stripe.Webhook.construct_event(
+      payload, sig_header, endpoint_secret
+    )
+  except ValueError as e:
+    # Invalid payload
+    return HttpResponse(status=400)
+    # Invalid signature
+    return HttpResponse(status=400)
+
+  # Handle the checkout.session.completed event
+  if event['type'] == 'checkout.session.completed':
+    session = event['data']['object']
+   
+    # Fetch all required data from session
+    client_reference_id = session.get('client_reference_id')
+    stripe_customer_id = session.get('customer')
+    stripe_subscription_id = session.get('subscription')
+
+    #Get user and create new StripeConsumer
+    user = User.objects.get(id=client_reference_id)
+    StripeCustomer.objects.create(
+      user = user,
+      stripeCustomerId = stripe_customer_id,
+      stripeSubscriptionId = stripe_subscription_id,
+    )
+    print(user.username + ' just subscribed.')
+  return HttpResponse(status=200)
