beb464f12a3f59ac7cfe2e0b2445e78eb8dd66f9
mutuajames <njorog3.james@gmail.com>
2023-01-03 12:08:14 +0300

    Added checkout session

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='subscriptions-home'),
+    path('config/', views.stripe_config, name='stripe-config'),
+    path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
 ]
